7dbbc3f3633efa6948440ec0c3c93b91bf92fbd3
Tanujairam <88611197+Tanujairam-TG@users.noreply.github.com>
2024-05-27 16:32:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
 INSTAGRAM_USERNAME = "your_instagram_username"
 INSTAGRAM_PASSWORD = "your_instagram_password"
 MONGODB_URI = "your_mongodb_uri"
+
+# Sakura.fm credentials
+SAKURA_USERNAME = "your_sakura_username"
+SAKURA_PASSWORD = "your_sakura_password"
