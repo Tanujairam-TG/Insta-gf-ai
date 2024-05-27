414570e088f72889c6f501e25b576f9790688481
Tanujairam <88611197+Tanujairam-TG@users.noreply.github.com>
2024-05-27 21:39:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ INSTAGRAM_PASSWORD = "tanu@23072008"
 MONGODB_URI = "mongodb+srv://tanujairam:tanujairam@whatsapp.yixkzgb.mongodb.net/?retryWrites=true&w=majority&appName=Whatsapp"
 
 # Sakura.fm credentials
-SAKURA_USERNAME = "your_sakura_username"
-SAKURA_PASSWORD = "your_sakura_password"
+SAKURA_USERNAME = "tanujairam"
+SAKURA_PASSWORD = "tanu@23072008"
