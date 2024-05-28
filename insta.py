beec436d23068ca322b04205b9a02c07c376dbac
Tanujairam <88611197+Tanujairam-TG@users.noreply.github.com>
2024-05-28 11:54:57 +0530

    Delete insta.py

--- insta.py
+++
@@ -1,9 +0,0 @@
-from instabot import Bot
-
-def login_to_instagram(username, password):
-    bot = Bot()
-    bot.login(username=username, password=password)
-    return bot
-
-# Example usage:
-# session = login_to_instagram("your_instagram_username", "your_instagram_password")
