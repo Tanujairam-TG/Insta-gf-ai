da4ddef033981b0445eb353e91211f0fd0a5f907
Tanujairam <88611197+Tanujairam-TG@users.noreply.github.com>
2024-05-28 11:54:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
+from instabot import Bot
+from sakura_fm import Client
 from config import *
-from insta import login_to_instagram
-from sakura import SakuraChatbot
+
+def login_to_instagram(username, password):
+    bot = Bot()
+    bot.login(username=username, password=password)
+    return bot
+
+class SakuraChatbot:
+    def __init__(self, username, password, mongo_uri):
+        self.client = Client(username=username, password=password, mongo=mongo_uri)
+
+    def send_message_to_sakura(self, uid, char_id, prompt):
+        response = self.client.sendMessage(uid, char_id, prompt)
+        return response["reply"]  # Extract the relevant part of the response
 
 def main():
     # Authenticate with Instagram
@@ -18,10 +31,10 @@ def main():
                 user_id = message["user_id"]
                 received_text = message["text"]
 
-                # Process the received message (you can customize this logic)
+                # Send the received message to Sakura.fm
                 response = sakura_bot.send_message_to_sakura(user_id, 'dmDCgmq', received_text)
 
-                # Send the response back to the user on Instagram
+                # Send the Sakura.fm response back to the user on Instagram
                 session.send_message(user_id, response)
         except KeyboardInterrupt:
             print("\nChatbot stopped by user.")
